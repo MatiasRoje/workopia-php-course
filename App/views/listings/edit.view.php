19573cf5c14c6437269f5a831e617173d5dbd899
MatiasRoje <rojechi@gmail.com>
2024-03-12 22:03:37 +0100

    missing crud functionalities added to the webpage: create, update and delete

--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -4,8 +4,9 @@
 
 <section class="flex justify-center items-center mt-20">
   <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-    <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
-    <form method="POST" action="/listings">
+    <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
+    <form method="POST" action="/listings/<?= $listing->id ?>">
+    <input type="hidden" value="PUT" name="_method">
       <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
         Job Info
       </h2>
@@ -22,7 +23,7 @@
           name="title"
           placeholder="Job Title"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["title"] ?? "" ?>"
+          value="<?= $listing->title ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -30,7 +31,7 @@
           name="description"
           placeholder="Job Description"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-        ><?= $listing["description"] ?? "" ?></textarea>
+        ><?= $listing->description ?? "" ?></textarea>
       </div>
       <div class="mb-4">
         <input
@@ -38,7 +39,7 @@
           name="salary"
           placeholder="Annual Salary"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["salary"] ?? "" ?>">
+          value="<?= $listing->salary ?? "" ?>">
         </div>
       <div class="mb-4">
         <input
@@ -46,7 +47,7 @@
           name="requirements"
           placeholder="Requirements"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["requirements"] ?? "" ?>"
+          value="<?= $listing->requirements ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -55,7 +56,7 @@
           name="benefits"
           placeholder="Benefits"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["benefits"] ?? "" ?>"
+          value="<?= $listing->benefits ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -64,7 +65,7 @@
           name="tags"
           placeholder="Tags"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["tags"] ?? "" ?>"
+          value="<?= $listing->tags ?? "" ?>"
         />
       </div>
       <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
@@ -76,7 +77,7 @@
           name="company"
           placeholder="Company Name"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["company"] ?? "" ?>"
+          value="<?= $listing->company ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -85,7 +86,7 @@
           name="address"
           placeholder="Address"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["address"] ?? "" ?>"
+          value="<?= $listing->address ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -94,7 +95,7 @@
           name="city"
           placeholder="City"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["city"] ?? "" ?>"
+          value="<?= $listing->city ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -103,7 +104,7 @@
           name="state"
           placeholder="State"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["state"] ?? "" ?>"
+          value="<?= $listing->state ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -112,7 +113,7 @@
           name="phone"
           placeholder="Phone"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["phone"] ?? "" ?>"
+          value="<?= $listing->phone ?? "" ?>"
         />
       </div>
       <div class="mb-4">
@@ -121,7 +122,7 @@
           name="email"
           placeholder="Email Address For Applications"
           class="w-full px-4 py-2 border rounded focus:outline-none"
-          value="<?= $listing["email"] ?? "" ?>"
+          value="<?= $listing->email ?? "" ?>"
         />
       </div>
       <button
@@ -130,7 +131,7 @@
         Save
       </button>
       <a
-        href="/"
+        href="/listings/<?= $listing->id ?>"
         class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none"
       >
         Cancel
--- public/index.php
+++ public/index.php
@@ -1,16 +1,9 @@
 <?php
+session_start();
 require __DIR__ . "/../vendor/autoload.php";
 require "../helpers.php";
 
 use Framework\Router;
-
-// NOTE this function would work but it's not appropriate for a professional enviroment
-// spl_autoload_register(function ($class) {
-//     $path = basePath("Framework/" . $class . ".php");
-//     if (file_exists($path)) {
-//         require $path;
-//     }
-// });
 
 // Instantiating the router
 $router = new Router();
